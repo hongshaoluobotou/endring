08763f8c17f1bd5381913b8167f2b690f087c5d6
hongshaoluobotou <635718249@qq.com>
2026-07-30 15:49:04 +0800

    终末之环的击退抗性+1

--- src/main/java/com/hongshaoluobotou/ModItems.java
+++ src/main/java/com/hongshaoluobotou/ModItems.java
@@ -32,6 +32,7 @@ public final class ModItems {
 			.add(Attributes.ARMOR, modifier("end_ring_armor", 3.0), EquipmentSlotGroup.HEAD)
 //			.add(Attributes.ARMOR_TOUGHNESS, modifier("end_ring_toughness", 1_000_000.0), EquipmentSlotGroup.HEAD)
 			.add(Attributes.ARMOR_TOUGHNESS, modifier("end_ring_toughness", 3.0), EquipmentSlotGroup.HEAD)
+			.add(Attributes.KNOCKBACK_RESISTANCE, modifier("end_ring_toughness", 0.1), EquipmentSlotGroup.HEAD)
 			.build();
 
 		Item.Properties properties = new Item.Properties()
